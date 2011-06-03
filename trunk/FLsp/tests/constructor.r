002c9c67abd8b5f6872e5fe703e21d789b426e13
drfinlayscott <drfinlayscott@683547ed-0558-4ddc-934d-89df8977f9be>
2011-06-03 14:54:48 +0000

    test iterations,problem with FLQuants

--- trunk/FLsp/tests/constructor.r
+++ trunk/FLsp/tests/constructor.r
@@ -25,16 +25,41 @@ index <- FLQuant(dat$cpue,dimnames=list(year=dat$year))
 test <- FLsp(catch=catch, index=FLQuants(i1 = index))
 test <- fitsp(test)
 p <- predict(test)
-ec <- evalC(test)
+evalC(test)
 biomass(test)
 qhat(test)
 sigma2(test)
 ll(test)
 
-
-# Test fixed
+# Test fixed - broken
 test <- fitsp(test, fixed=list(k = 1.4e5))
 
+# Iter tests
+# index with multiple iters
+# catch with miters
+# catch and index with multiple iters
+# r and k with multiple iters
+iters <- 10
+indexit <- propagate(index,iters)
+indexit <- indexit * rlnorm(prod(dim(indexit)),0,0.2)
 
+catchit <- propagate(catch,iters)
+catchit <- catchit * rlnorm(prod(dim(catchit)),0,0.2)
+
+test1 <- FLsp(catch=catch, index=FLQuants(indexit))
+test2 <- FLsp(catch=catchit, index=FLQuants(index))
+test3 <- FLsp(catch=catchit, index=FLQuants(indexit))
+
+test1 <- fitsp(test1)
+test2 <- fitsp(test2)
+test3 <- fitsp(test3)
+
+hist(c(params(test1)["r"]))
+hist(c(params(test2)["r"]))
+hist(c(params(test3)["r"]))
+
+# Problem is iter and index FLQuants
+evalC(test1)
+biomass(test1)
 
 
