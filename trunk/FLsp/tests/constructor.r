9439707854e5d189c4d08feac05b9845da7ab207
drfinlayscott <drfinlayscott@683547ed-0558-4ddc-934d-89df8977f9be>
2011-06-03 11:25:51 +0000

    accessor methods added

--- trunk/FLsp/tests/constructor.r
+++ trunk/FLsp/tests/constructor.r
@@ -1,30 +1,40 @@
 # FLsp constructor test
 #setwd("c:/Projects/Deepfishman/deepfishman")
-setwd("m:/Projects/Deepfishman/deepfishman")
-#dat <- read.csv("trunk/FLsp/data/NZRL.csv")
-dat <- read.csv("FLsp/data/NZRL.csv")
+#setwd("m:/Projects/Deepfishman/deepfishman")
+setwd("~/Work/deepfishman")
+dat <- read.csv("trunk/FLsp/data/NZRL.csv")
+#dat <- read.csv("FLsp/data/NZRL.csv")
 library(FLsp)
 
 catch <- FLQuant(dat$catch,dimnames=list(year=dat$year))
 index <- FLQuant(dat$cpue,dimnames=list(year=dat$year))
 
-test <- FLsp() # should return an empty object?
-# Write the model function - returns ll etc
-test <- FLsp(model=sp) # Fails as no catch or index (needed for validity check)
+#test <- FLsp() # should return an empty object?
+## Write the model function - returns ll etc
+#test <- FLsp(model=sp) # Fails as no catch or index (needed for validity check)
 
 
 # Sets model as sp by default
-test <- FLsp(catch=catch, index=FLQuants(i1 = index))
+#test <- FLsp(catch=catch, index=FLQuants(i1 = index))
 
 # Why does no longer work?
-test <- FLsp(catch=catch,index=FLQuants(i1=index),model=sp)
+#test <- FLsp(catch=catch,index=FLQuants(i1=index),model=sp)
 
 #test@logl
 
 test <- FLsp(catch=catch, index=FLQuants(i1 = index))
 test <- fitsp(test)
 p <- predict(test)
+ec <- evalC(test)
+biomass(test)
+qhat(test)
+sigma2(test)
+ll(test)
 
 
-# fails
+# Test fixed
 test <- fitsp(test, fixed=list(k = 1.4e5))
+
+
+
+
