84490cb1104c43e336ac1597952134e21e5755e5
drfinlayscott <drfinlayscott@683547ed-0558-4ddc-934d-89df8977f9be>
2011-06-23 17:06:10 +0000

    added profile to code and manual

--- FLsp/R/FLsp.r
+++ FLsp/R/FLsp.r
@@ -243,4 +243,178 @@ setMethod("dims", signature(obj="FLsp"),
 	})
 
 
+# New profile plot - includes gradients
+setMethod("profile", signature(fitted="FLsp"),
+  function(fitted, which, maxsteps=11, range=0.5, ci=c(0.25, 0.5, 0.75, 0.95),
+      plot=TRUE, fixed=list(), print=FALSE, control=list(trace=0), ...)
+  {
+    # vars
+    foo <- logl(fitted)
+    params <- params(fitted)
+    parnames <- dimnames(params)$params
+    fixnames <- names(fixed)
+    profiled <- list()
+    grid <- list()
+    plotfit <- TRUE
+
+    # HACK! clean up fixed list if elements are named vectors
+    fixed <- lapply(fixed, function(x){ names(x) <- NULL; x})
+
+    # which params to profile
+    if(missing(which))
+      which <- parnames[!parnames %in% fixnames]
+    if(length(which) > 2)
+        stop("surface only works over 2 parameters")
+    
+    # data
+    args <- list()
+    data <- names(formals(foo))
+    data <- data[data %in% slotNames(fitted)]
+    for(i in data)
+      args[i] <- list(slot(fitted, i))
+      
+    # use initial if model has not been estimated
+    if(all(is.na(params)))
+    {
+      params <- do.call(initial(fitted), args)
+      plotfit <- FALSE
+    }
+
+    # (1) create grid of param values for numeric range
+    if(is.numeric(range) && length(range) == 1)
+    {
+      if(!plotfit)
+        warning("model has not been fitted: initial values are used for profile range")
+      for(i in which)
+      {
+        # steps for param[i]
+        estim <- c(params[i,])
+        steps <- estim * seq(1-range, 1+range, length=maxsteps)
+        profiled[[i]] <- sort(steps)
+      }
+    # (2) and for list of ranges
+    } else if (is.list(range)) 
+    {
+      # if missing(which), which is names in range
+      if(missing(which))
+        which <- names(range)
+      else
+        # checks all params to be profiled specified
+        if(any(names(range) != which))
+          stop("range not specified for parameters:", which[!which%in%names(range)])
+      profiled <- lapply(range, sort)
+    }
+
+    # grid
+    grid <- do.call(expand.grid, profiled)
+
+    # col for logLik
+    grid$logLik <- as.numeric(NA)
+
+    dots <- list(...)
+    # calculate logLik for grid if no fitting
+    if(identical(order(c(which, fixnames)), order(parnames)))
+      for(i in seq(nrow(grid)))
+        grid[i, 'logLik'] <- do.call(logl(fitted), c(args, as.list(grid[i,which]), fixed))
+
+    # or fit over grid
+    else
+      for(i in seq(nrow(grid)))
+      {
+        fixed <- as.list(grid[i,which])
+        names(fixed) <- which
+        grid[i, 'logLik'] <- do.call('fmle', c(list(object=fitted, fixed=fixed,
+          control=control), dots))@logLik
+      }
+   
+    surface <- tapply(grid$logLik, grid[,which], sum)
+
+    # print
+    if(print)
+    {
+      cat(paste("max(profile) =", format(max(grid$logLik), digits=5), " "))
+      for(i in which)
+        cat(paste(i, " = ", format(grid[grid$logLik==max(grid$logLik),i], digits=5), " "))
+      cat("\n")
+      if(plotfit)
+      {
+        cat(paste("logLik =", format(logLik(fitted), digits=5), " "))
+        for(i in which)
+          cat(paste(i, " = ", format(c(params(fitted)[i]), digits=5), " "))
+        cat("\n")
+      }
+    }
+
+    # CIs
+    cis <- max(surface) - qchisq(ci, 2)
+    
+    # plot
+    if(plot)
+    {
+      if(length(which) == 2)
+      {
+
+    profiled[["ll_grad_k"]] <- rep(NA,length(profiled[[1]]))
+    profiled[["ll_grad_r"]] <- rep(NA,length(profiled[[1]]))
+    dummy <- fitted
+    dummy@params <- params
+    # get dll/dr
+    for (i in 1:length(profiled[["ll_grad_k"]]))
+    {
+	dummy@params["r",] <- profiled[["r"]][i]
+	profiled[["ll_grad_r"]][i] <- evalC(iter(dummy,1))[["ll_grad_r"]]
+    }
+    dummy@params <- params
+    # get dll/dk
+    for (i in 1:length(profiled[["ll_grad_k"]]))
+    {
+	dummy@params["k",] <- profiled[["k"]][i]
+	profiled[["ll_grad_k"]][i] <- evalC(iter(dummy,1))[["ll_grad_k"]]
+    }
+    #browser()
+    # Need y and x lims for the plots
+    ylim= profiled[["ll_grad_r"]][round(maxsteps/2)+1]*c(-3,3)
+    xlim= profiled[["ll_grad_k"]][round(maxsteps/2)+1]*c(-3,3)
+
+    # Now set up plot
+    lay_mat <- matrix(c(1,3,0,2),nrow=2)
+    lay<-layout(lay_mat,widths=c(3,1),heights=c(1,3))
+    #layout.show(lay)
+    # And fanny about with margins
+    par(mar=c(0,5.1,1,1))
+    plot(x=profiled[["r"]], y=profiled[["ll_grad_r"]], type="l", axes=FALSE, xlab="",ylab="dll/dr", ylim=ylim)
+    lines(x=c(-1e9,1e9),y=c(0,0),lty=2)
+    par(mar=c(5.1,0,1,1))
+    plot(y=profiled[["k"]], x=profiled[["ll_grad_k"]], type="l", axes=FALSE, ylab= "", xlab="dll/dr",xlim=xlim)
+    lines(y=c(-1e9,1e9),x=c(0,0),lty=2)
+    par(mar=c(5.1,5.1,1,1))
+
+
+        do.call('image', c(list(x=profiled[[1]], y=profiled[[2]], z=surface,
+          xlab=which[1], ylab=which[2]), dots[!names(dots) %in% names(formals(optim))]))
+
+        if(plotfit)
+          points(params[which[1]], params[which[2]], pch=19)
+
+        do.call('contour', list(x=sort(profiled[[1]]), y=sort(profiled[[2]]), z=surface,
+          levels=cis, add=TRUE, labcex=0.8, labels=ci))
+      }
+      else if(length(which) == 1)
+      {
+        plot(grid[,which], grid[,'logLik'], type='l', xlab=which, ylab="logLik", axes=F)
+        axis(1); box()
+        points(params[which], logLik(fitted), pch=19)
+      }
+    }
+    if(length(which) == 2)
+      invisible(list(x=grid[,which[1]], y=grid[,which[2]], z=surface))
+    else if(length(which) == 1)
+      invisible(list(x=grid[which], y=grid['logLik']))
+  }
+) # }}}
+
+
+
+
+
 
