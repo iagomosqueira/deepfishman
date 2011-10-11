898984170f1ab4f343804bb4f52f4722a0d7dca9
drfinlayscott@gmail.com <drfinlayscott@gmail.com@683547ed-0558-4ddc-934d-89df8977f9be>
2011-10-11 15:23:42 +0000

    testing log hessian

--- trunk/FLsp/tests/test_hessian.r
+++ trunk/FLsp/tests/test_hessian.r
@@ -21,8 +21,34 @@ sigma <- 0.207
 tape_all <- .Call("flspCpp_tape",data$catch,data$cpue,r,p,k)
 tape_all_log <- .Call("flspCpp_tape_log",data$catch,data$cpue,r,p,k)
 
+# Check gradients
+tiny <- 1e-9
+ll_orig <- .Call("flspCpp_tape",data$catch,data$cpue,r,p,k)[["ll"]]
+ll_r <- .Call("flspCpp_tape",data$catch,data$cpue,r+tiny,p,k)[["ll"]]
+ll_k <- .Call("flspCpp_tape",data$catch,data$cpue,r,p,k+tiny)[["ll"]]
+dlldr <- (ll_r - ll_orig) / tiny
+dlldk <- (ll_k - ll_orig) / tiny
+
+dlldr
+tape_all[["ll_grad_r"]]
+
+dlldk
+tape_all[["ll_grad_k"]]
+
+# Check log gradients
+ll_r_log <- .Call("flspCpp_tape",data$catch,data$cpue,exp(log(r)+tiny),p,k)[["ll"]]
+ll_k_log <- .Call("flspCpp_tape",data$catch,data$cpue,r,p,exp(log(k)+tiny))[["ll"]]
+dlldr_log <- (ll_r_log - ll_orig) / tiny
+dlldk_log <- (ll_k_log - ll_orig) / tiny
+
+dlldr_log
+tape_all_log[["ll_grad_r"]]
+
+dlldk_log
+tape_all_log[["ll_grad_k"]]
 
 
+# Approximate hessian of non logged
 # returns the gradients
 simple_diff <- function(r,k)
 {
@@ -30,14 +56,10 @@ simple_diff <- function(r,k)
 	return(c(llorig[["ll_grad_r"]],llorig[["ll_grad_k"]]))
 }
 
-simple_diff(r,k)
-
-# Approximate hessian of non logged
-tiny <- 1e-9
 dorig <- simple_diff(r,k) # original gradients
 drtiny <- simple_diff(r+tiny,k)
 dktiny <- simple_diff(r,k+tiny)
-dll2dr2 <- (drtiny[1]-dorig[1])/tiny
+dll2dr2 <- (drtiny[1]-dorig[1])/tiny # change in grad_ll with change in r
 dll2dk2 <- (dktiny[2]-dorig[2])/tiny
 dll2drk <-(dktiny[1]-dorig[1])/tiny
 dll2dkr <-(drtiny[2]-dorig[2])/tiny
@@ -48,19 +70,18 @@ hesshat <- matrix(c(dll2dr2,dll2drk,dll2dkr,dll2dk2),nrow=2)
 tape_all$hessian
 hesshat
 
-# Check gradient of logged function
-
 # Hessian of logged
+# We have checked that the tape_log function returns dll / d log(r)
 simple_diff_log <- function(r,k)
 {
 	llorig <- .Call("flspCpp_tape_log",data$catch,data$cpue,r,p,k)
 	return(c(llorig[["ll_grad_r"]],llorig[["ll_grad_k"]]))
 }
 
-tiny <- 1e-6
+tiny <- 1e-9
 dorig <- simple_diff_log(r,k) # original gradients
-drtiny <- simple_diff_log(r+tiny,k)
-dktiny <- simple_diff_log(r,k+tiny)
+drtiny <- simple_diff_log(exp(log(r)+tiny),k)
+dktiny <- simple_diff_log(r,exp(log(k)+tiny))
 dll2dr2 <- (drtiny[1]-dorig[1])/tiny
 dll2dk2 <- (dktiny[2]-dorig[2])/tiny
 dll2drk <-(dktiny[1]-dorig[1])/tiny
@@ -69,3 +90,5 @@ hesshat <- matrix(c(dll2dr2,dll2drk,dll2dkr,dll2dk2),nrow=2)
 
 tape_all_log$hessian
 hesshat
+
+# looks fine
