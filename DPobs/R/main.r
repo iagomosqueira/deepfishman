95417a0ce07a117c656bebc649147f9bf34930e9
drfinlayscott@gmail.com <drfinlayscott@gmail.com@683547ed-0558-4ddc-934d-89df8977f9be>
2011-10-26 09:54:23 +0000

    Added obs error work with Charlie

---
+++ DPobs/R/main.r
@@ -0,0 +1,8 @@
+# The main loop
+
+# Load the preconditioned stock
+# Set parameters (obs error, srr error, nyrs etc)
+# Loop over years and run fowards
+# Calc the performance stats
+
+
---
+++ DPobs/R/make_stocks.r
@@ -0,0 +1,9 @@
+# Conditioning the stocks
+
+# Set up the stock using FLH
+
+
+# Project forward under some fishing scenario
+
+
+# Save it
