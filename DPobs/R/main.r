54265e5cbfbd3d3aa8514f9afac5b717ebd34142
drfinlayscott@gmail.com <drfinlayscott@gmail.com@683547ed-0558-4ddc-934d-89df8977f9be>
2011-10-26 13:27:45 +0000

    Got the perfect knowledge scenario working

--- DPobs/R/main.r
+++ DPobs/R/main.r
@@ -5,4 +5,72 @@
 # Loop over years and run fowards
 # Calc the performance stats
 
+#*******************************************************************************
+rm(list=ls())
+library(FLCore)
+library(FLash)
+library(FLAssess)
+library(FLsp)
 
+#*******************************************************************************
+setwd("c:/Projects/Deepfishman/deepfishman/trunk/DPobs/R")
+source("hcr_func.r")
+
+# Load the conditioned stock
+source("make_stocks.r")
+
+#*******************************************************************************
+# Set some parameters
+obs_cv <- 0.6
+srr_sd <- 0
+niters <- 1
+projyrs <- 20
+
+#Blim <- 0
+
+hcryrs <- (dims(stk1)$year+1) : (dims(stk1)$year+projyrs)
+
+#*******************************************************************************
+# Set up the stk for projection
+# Including future weights and all that gubbins
+stk_true <- stf(stk1,nyears=projyrs)
+# Clean out harvest to avoid confusion
+harvest(stk_true)[,projyrs] <- NA
+#stk_true <- window(stk1,end=dims(stk1)$year+projyrs)
+stk_true <- propagate(stk_true,niters)
+
+# To store output of the HCR
+TAC <- array(NA,dim=c(niters,dims(stk_true)$year))
+
+index_sp <- stock(stk_true)
+index_sp <- index_sp * rlnorm(prod(dim(index_sp)),0,obs_cv)
+
+for (yr in hcryrs)
+{
+  #if (yr==20) browser() # something weird going on...
+
+  cat("yr: ", yr, "\n")
+  # Get new TAC
+  # Assess using last years catch and index data
+  # No error on catch
+  catch_sp <- catch(stk_true)[,1:(yr-1)]
+  index_sp[,yr-1] <- stock(stk_true)[,yr-1] * rlnorm(niters,0,obs_cv)
+  # Plus some obs error!
+  #index_sp * rlnorm(prod(dim(index_sp)),0,obs_cv)
+  cat("index: ", index_sp, "\n")
+  
+  flsp <- FLsp(catch=catch_sp,index=window(index_sp, start=1,end=yr-1))
+  flsp <- fitsp(flsp,lower=c(1e-6,10),upper=c(10,1e9),control=DEoptim.control(itermax=1500,trace=500))
+  Bcurrent <- bcurrent(flsp)
+  TAC[,yr] <- hcr(B=c(Bcurrent), r= c(params(flsp)['r',]), k= c(params(flsp)['k',]), Blim=0.9*Bmsy(flsp))
+  cat("New TAC: ", TAC[,yr], "\n")
+
+  # Project stock using TAC
+  ctrl <- fwdControl(data.frame(year=yr, quantity="catch",val=TAC[,yr]))
+  stk_true <- fwd(stk_true, ctrl=ctrl, sr=list(model=model(cod1), params=params(cod1)))
+
+}
+
+plot(stk_true)
+# final estimate of msy
+Msy(flsp)
--- DPobs/R/make_stocks.r
+++ DPobs/R/make_stocks.r
@@ -1,9 +1,75 @@
 # Conditioning the stocks
 
 # Set up the stock using FLH
-
-
 # Project forward under some fishing scenario
-
-
 # Save it
+
+
+library(FLsp)
+library(FLH)
+library(ggplot2)
+
+#*******************************************************************************
+# Make a codoid
+Linf <- 120
+maxage <- 25
+k <- exp(0.5235792+log(Linf)*-0.4540248) # where did these numbers come from?
+# selectivity - sort of dome shaped
+a1 <- 0.5
+sL <- 0.5
+sR <- 5
+# maturity
+mat95 <- 6
+# SRR
+s <- 0.75
+v <- 1e3
+# Make the stock
+cod1 <- genBRP(age=1:maxage, Linf=Linf, k=k, a1=a1, sL=sL, sR=sR,
+                mat95=mat95, s=s, v=v)
+
+# Check it out
+ggplot(as.data.frame(catch.sel(cod1))) + geom_line(aes(age,data)) + ylab("Catch selectivity") + xlab("Age")
+
+#*******************************************************************************
+# Set up fishing history
+maxt <- 10
+fmsy <- c(refpts(cod1)[,'harvest'])[4]
+maxFfactor <- 2
+maxF <- maxFfactor * fmsy
+
+# One way trip
+#F1 <- maxF*sin(seq(from=0,to=pi/2,length=maxt))[-1]
+
+# Some contrast
+#F1 <- maxF*sin(seq(from=0,to=pi,length=maxt))[-1]
+
+# Constant at a high level of F - if maxF = 3*fmsy, almost dead
+F1 <- rep(maxF,maxt-1)
+
+# Add some noise to F to make it interesting
+Fnoise_sd <- 0
+F1 <- F1 * rlnorm(length(F1),0,Fnoise_sd)
+
+# Plot fishing history
+ggplot(data.frame(time=2:maxt, F=F1)) + geom_line(aes(x=time,y=F)) +
+	geom_line(aes(x=time,y=FMSY),data=data.frame(time=2:maxt, FMSY=fmsy)) +
+	scale_y_continuous(limits = c(0,maxF*1.3))
+	
+#*******************************************************************************
+# Project stock forward
+
+# Noise level of SRR
+srr_sd <- 0.0
+
+# Set up fwd()
+stk1 <- as(cod1, 'FLStock')
+stk1 <- window(stk1,end=maxt)
+ctrl_F1 <- fwdControl(data.frame(year=2:maxt, quantity="f",val=F1))
+srr_residuals <- FLQuant(rlnorm(dims(stk1)$year * dims(stk1)$iter,0,srr_sd),
+									dimnames=list(age=1,year=dimnames(stock.n(stk1))$year,iter=dimnames(stock.n(stk1))$iter))
+stk1 <- fwd(stk1, ctrl=ctrl_F1, sr=list(model=model(cod1), params=params(cod1)), sr.residuals=srr_residuals)
+
+print(plot(stk1))
+
+#*******************************************************************************
+
