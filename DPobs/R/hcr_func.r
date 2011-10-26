e30ff68b7f757a02ba1eb588d22606c7e5f3ba62
drfinlayscott@gmail.com <drfinlayscott@gmail.com@683547ed-0558-4ddc-934d-89df8977f9be>
2011-10-26 11:50:04 +0000

    Wrote HCR function

--- DPobs/R/hcr_func.r
+++ DPobs/R/hcr_func.r
@@ -1,10 +1,24 @@
 # The HCR function
 
-hcr <- function(Bcurrent,r,k,Blim)
+hcr <- function(B,r,k,Blim)
 {
+  catch <- rep(NA,length(B))
+  Bmsy <- k/2
+  Msy <- r*k/4
 
-#catch <- something
-#return(catch)
-
+  # equation for lower bit of HCR
+  m <- Msy / (Bmsy - Blim)
+  x <- Msy - m * Bmsy
+  catch[B > Bmsy] <- (B * (Msy / Bmsy))[B > Bmsy]
+  catch[B <= Bmsy] <- (B * m + x)[B <= Bmsy]
+  catch[catch<0] <- 0
+  return(catch)
 }
 
+#Blim <- 300
+#Bvec <- seq(from=0,to=1000,length=50)
+#r <- rep(0.5,length(Bvec))
+#k <- rep(1000,length(Bvec))
+
+#C <- hcr(Bvec,r,k,Blim)
+#plot(Bvec,C,type="l")
